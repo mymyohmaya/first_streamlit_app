be1f6813315ef5b1a574841686baf804883f7494
mymyohmaya <96263198+mymyohmaya@users.noreply.github.com>
2022-08-04 11:49:06 -0700

    Update streamlit_app.py
    Fixed emoji error next to kale

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text(' 🥣Omega 3 and Blueberry Oatmeal')
-streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free Ranger Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
