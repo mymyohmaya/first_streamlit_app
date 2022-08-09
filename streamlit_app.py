ad413088bc2bc947a7f8e6aa4f3d2bc516bacde1
mymyohmaya <96263198+mymyohmaya@users.noreply.github.com>
2022-08-09 13:45:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Stefs Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text(' 🥣Omega 3 and Blueberry Oatmeal')
